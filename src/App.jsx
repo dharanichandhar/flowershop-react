e35973dd6f451e75d369f0a79e4b7e4cc44bc9ec
dharanichandhar <dharaniv2308@gmail.com>
2025-07-23 04:09:11 +0000

    Update Many

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import React from 'react'
-import Demo from './Demo'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
-
-import Navbar from './components/Navbar'
-import { BrowserRouter, Routes , Route} from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Gallery from './components/Gallery'
-import Contact from './components/Contact'
-import Login from './components/Login'
-import Signup from './components/Signup'
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
       <Navbar />
-        <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/gallery' element={<Gallery />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} /> {/* ✅ Fix: add this route */}
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
